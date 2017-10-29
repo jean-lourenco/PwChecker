79da7ee084b9b3ae65714fb58164af12c82ea4c2
Jean Lourenço <jeancarlos.bnu@gmail.com>
2017-10-29 18:59:05 -0200

    build scripts added

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#exit if any command fails
+set -e
+
+artifactsFolder="./artifacts"
+
+if [ -d $artifactsFolder]; then
+	rm -R $artifactsFolder
+fi
+
+dotnet restore
+
+#dotnet test -c Release
+
+revision=${TRAVIS_JOB_ID:=1}
+revision=$(printf "%04d" $revision)
+
