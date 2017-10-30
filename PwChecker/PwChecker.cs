a86d50a7ab0c161fc3c7aedd3a479839783f2710
Jean Lourenço <jeancarlos.bnu@gmail.com>
2017-10-30 21:26:06 -0200

    added throttle handling

--- PwChecker/PwChecker.cs
+++ PwChecker/PwChecker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace PwCheckerLib
     {
         private const string BaseUrl = "https://haveibeenpwned.com/api/v2/pwnedpassword";
         private static readonly HttpClient Client = new HttpClient();
+        private static DateTime LastExecution = DateTime.MinValue;
+        private static readonly TimeSpan ApiThrottle = TimeSpan.FromMilliseconds(1600);
 
         static PwChecker()
         {
@@ -20,9 +23,28 @@ namespace PwCheckerLib
             var data = new FormUrlEncodedContent(new[] {
                 new KeyValuePair<string, string>("Password", password )
             });
+
+            await WaitForThrottle();
+
+            LastExecution = DateTime.Now;
             var response = await Client.PostAsync(BaseUrl, data);
 
+            if ((int)response.StatusCode == 429)
+                throw new HttpRequestException("Too Many Requests (429) on 'haveibeenpwned.com' api");
+
+            if (!response.IsSuccessStatusCode)
+                throw new HttpRequestException($"{response.StatusCode} on 'haveibeenpwned.com' api");
+
             return response.StatusCode == HttpStatusCode.OK;
+        }
+
+        private static async Task WaitForThrottle()
+        {
+            var now = DateTime.Now;
+            var nextPossibleExecution = now + ApiThrottle;
+
+            if (nextPossibleExecution > now)
+                await Task.Delay(nextPossibleExecution - now);
         }
     }
 }
